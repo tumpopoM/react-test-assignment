7cd9c99b3808cd0b5781b73c6f4de30b82dcd479
tumpopoM <wijitrarattanason@gmail.com>
2026-04-16 16:48:20 +0700

    feat: add form to create item

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Form, Input, Button, InputNumber } from 'antd'
+import { useDispatch } from 'react-redux'
+import { addItem } from './store/itemSlice'
+import { v4 as uuidv4 } from 'uuid'
+
+type FormValues = {
+  name: string
+  age: number
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [form] = Form.useForm()
+  const dispatch = useDispatch()
+
+  const onFinish = (values: FormValues) => {
+    dispatch(
+      addItem(
+        {
+          id: uuidv4(),
+          name: values.name,
+          age: values.age,
+        }
+      )
+    )
+    form.resetFields()
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div style={{ padding: 20}}>
+      <h2>Add Item</h2>
 
-      <div className="ticks"></div>
+        <Form form={form} layout="vertical" onFinish={onFinish}>
+        <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <Form.Item name="age" label="Age" rules={[{ required: true }]}>
+          <InputNumber style={{ width: '100%' }} />
+        </Form.Item>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+
+      </Form>
+    </div>
   )
 }
 
