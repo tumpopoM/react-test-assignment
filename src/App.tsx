286edba2bac8da30cc97477226b904eddc61aaaf
tumpopoM <wijitrarattanason@gmail.com>
2026-04-16 16:59:14 +0700

    feat: implement edit item functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,82 @@
-import { Form, Input, Button, InputNumber } from 'antd'
-import { useDispatch } from 'react-redux'
-import { addItem } from './store/itemSlice'
+import { Form, Input, Button, InputNumber, Table, Space } from 'antd'
+import { useState } from 'react'
+import {   useSelector,useDispatch } from 'react-redux'
+import { addItem, deleteItem, updateItem } from './store/itemSlice'
 import { v4 as uuidv4 } from 'uuid'
+import type { RootState} from './store/store'
+
 
 type FormValues = {
   name: string
   age: number
 }
 
+
+
 function App() {
   const [form] = Form.useForm()
   const dispatch = useDispatch()
-
+  const [editingItemId, setEditingItemId] = useState<string | null>(null)
   const onFinish = (values: FormValues) => {
-    dispatch(
-      addItem(
-        {
+   if (editingItemId) {
+       dispatch(
+        addItem(
+          {
+            id: uuidv4(),
+            name: values.name,
+            age: values.age,
+          }
+        )
+      )
+      setEditingItemId(null)
+    } else {
+      dispatch(
+        addItem({
           id: uuidv4(),
           name: values.name,
           age: values.age,
-        }
+        })
       )
-    )
+    }
     form.resetFields()
   }
+
+  const items = useSelector((state: RootState) => state.items.items)
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_: any, record: any) => (
+        <Space size="middle">
+            <Button type="primary" onClick={() => {
+              setEditingItemId(record.id)
+              form.setFieldsValue({
+                name: record.name,
+                age: record.age,
+              })
+              setEditingItemId(record.id)
+            }}>
+            Edit
+          </Button>
+
+          <Button type="primary" danger onClick={() => dispatch(deleteItem(record.id))}>
+            Delete
+          </Button>
+        </Space>
+      ),
+    },
+  ]
 
   return (
     <div style={{ padding: 20}}>
@@ -43,6 +96,9 @@ function App() {
         </Button>
 
       </Form>
+
+      <h2 style={{ marginTop: 40 }}>Items List</h2>
+      <Table dataSource={items} columns={columns} rowKey="id" />
     </div>
   )
 }
